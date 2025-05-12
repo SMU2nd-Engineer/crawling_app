bec13c9697d591ca48c3ddd2648b0429b6df8304
이가현 <gahyun970121@gmail.com>
2025-05-12 19:22:01 +0900

    티켓링크
    모듈화 완료

---
+++ crawling-master/ticketlink_crawling/perfomance_crawler.py
@@ -0,0 +1,142 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+from utils import create_driver
+from config import CATEGORY_DICT, GENRE_NUM_LIST
+
+def crawlPerformance():
+    driver = create_driver(headless=False)
+    url = "https://www.ticketlink.co.kr/local"
+    driver.get(url)
+    time.sleep(2)
+
+    detailDataList = []
+    maxTry = 50
+
+    listLength = {}
+
+    countryLength = driver.find_elements(By.XPATH, '//*[@id="content"]/section[1]/div/div/div/ul/li/button')
+
+    for countryNum in range(1, len(countryLength) + 1):
+        countryButton = driver.find_element(By.XPATH, f'//*[@id="content"]/section[1]/div/div/div/ul/li[{countryNum}]/button')
+        countryName = driver.find_element(By.CSS_SELECTOR, f'section.common_section.section_list_title > div > div > div > ul > li:nth-child({countryNum}) > button').text
+        countryButton.click()
+        time.sleep(1)
+
+        for _ in range(maxTry):
+            body = driver.find_element(By.TAG_NAME, 'body')
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(1)
+
+        for genreNum in GENRE_NUM_LIST:
+            listLength[genreNum] = len(driver.find_elements(By.XPATH, f'//*[@id="genre_{genreNum}"]/div/div/ul/li'))
+            print(f"지역: {countryName}, 카테고리: {CATEGORY_DICT.get(genreNum, '기타')}, 항목 수: {listLength[genreNum]}")
+
+        # 카테고리 버튼 한 번씩 클릭
+        for locationNum in range(1, len(GENRE_NUM_LIST) + 1):
+          for liNum in range(1, listLength[GENRE_NUM_LIST[locationNum - 1]] + 1):
+            tabButton = driver.find_element(By.XPATH, f'//*[@id="content"]/section[2]/div[1]/div/ul/li[{locationNum}]/button')
+            tabButton.click() 
+            time.sleep(1)
+
+            # 스크롤 위치 저장
+            scrollHeight = driver.execute_script("return window.pageYOffset;")
+            time.sleep(1)
+
+            for _ in range(0, int(((liNum - 1)/4))):
+              scrollHeight += 546
+              driver.execute_script(f"window.scrollTo(0, {scrollHeight})")
+              time.sleep(1)
+
+            # 현재 스크롤 위치 확인
+            # print(f"현재 스크롤 위치는 : {scrollHeight} 입니다.")
+
+            # 저장된 스크롤 위치로 이동
+            driver.execute_script(f"window.scrollTo(0, {scrollHeight})")
+            time.sleep(2)
+
+            try:
+              selectedElement = driver.find_element(By.XPATH, f'//*[@id="genre_{GENRE_NUM_LIST[locationNum-1]}"]/div/div/ul/li[{liNum}]')
+              # driver.execute_script("arguments[0].scrollIntoView();", element)  # 보이게 스크롤
+              time.sleep(2)
+
+              # 상세정보 클릭
+              selectedElement.click()
+              time.sleep(2)
+
+              # 팝업 창 지우기
+              try:
+                popup = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div[3]/button')
+                popup.click()
+              except:
+                popup = None
+
+              link = driver.current_url
+
+              #필요한 정보 가져오기
+              driver.implicitly_wait(2)
+              title = driver.find_element(By.CSS_SELECTOR, ".product_title").text
+              image = driver.find_element(By.CSS_SELECTOR, ".product_detail_img").get_attribute("src")
+              artistList =  driver.find_elements(By.CSS_SELECTOR, "div.product_casting > div > ul > li > a > span.product_casting_name")
+              priceList = driver.find_elements(By.CSS_SELECTOR, "div.product_info > ul:nth-child(3) > li:nth-child(1) > div > ul > li")
+              category = CATEGORY_DICT.get(GENRE_NUM_LIST[locationNum - 1], "기타")
+              etcList = driver.find_elements(By.CSS_SELECTOR, "#content > section.common_section.section_product_content > div > section > div:nth-child(3)")
+
+              info = driver.find_elements(By.CSS_SELECTOR, "div.product_info > ul > li > span")
+              for idx, i in enumerate(info, start=1):  # idx는 1부터 시작
+                label = i.text.strip()  # span 안에 들어있는 텍스트 가져오기
+
+                if label == "장소":
+                    place = driver.find_element(By.CSS_SELECTOR, f"div.product_info > ul > li:nth-child({idx}) button").text
+
+                elif label == "관람시간":
+                    runningTime = driver.find_element(By.CSS_SELECTOR, f"div.product_info > ul > li:nth-child({idx}) div").text
+
+                elif label == "기간":
+                    date = driver.find_element(By.CSS_SELECTOR, f"div.product_info > ul > li:nth-child({idx}) div").text
+                    sdate = date.split('-')[0].strip()
+                    edate = date.split()[-1].strip()
+
+                elif label == "관람등급":
+                    age = driver.find_element(By.CSS_SELECTOR, f"div.product_info > ul > li:nth-child({idx}) div").text
+
+              prices = [priceText.text for priceText in priceList]
+              price = '/'.join(prices)
+
+              artistNames = [artistText.text for artistText in artistList]
+              artist = '/'.join(artistNames)
+
+              etcs = [etcText.text for etcText in etcList]
+              etc = '/'.join(etcs)
+
+
+              detailData = {
+                  'title': title,
+                  'company': "티켓링크",
+                  'link': link,
+                  'sdate': sdate,
+                  'edate': edate,
+                  'place': place,
+                  'price': price,
+                  'grade': age,
+                  'cast': artist,
+                  'runningtime': runningTime,
+                  'img': image,
+                  'genre': category,
+                  'etc': etc
+              }
+
+              print(detailData)
+
+              detailDataList.append(detailData)
+              driver.back()
+              time.sleep(1)
+
+            except Exception as e:
+              print(f"에러 발생: {e}")
+              driver.get(url)
+              time.sleep(2)
+
+    driver.quit()
+    return pd.DataFrame(detailDataList)
---
+++ crawling-master/ticketlink_crawling/sports_crawler.py
@@ -0,0 +1,94 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+from utils import create_driver, get_current_year_month
+
+def crawlSports(driver, teams: dict, genre: str) -> pd.DataFrame:
+    driver = create_driver()
+    currentYear, currentMonth = get_current_year_month()
+
+    detailDataList = []
+    maxTry = 3
+
+    for team_url, team_name in teams.items():
+        url = "https://www.ticketlink.co.kr" + team_url
+        print(f"접속 중: {team_name} -> {url}")
+        driver.get(url)
+        time.sleep(2)
+
+        for _ in range(maxTry):
+            body = driver.find_element(By.TAG_NAME, 'body')
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(1)
+
+        try:
+            matches = driver.find_elements(By.CSS_SELECTOR, "div.reserve_lst_bx > ul > li")
+            print(f"{team_name} 경기 수: {len(matches)}개")
+
+            for match in range(1, len(matches) + 1):
+                try:
+                    time.sleep(2)
+                    importedDate = driver.find_element(By.CSS_SELECTOR, f"div.reserve_lst_bx > ul > li:nth-child({match}) > div.match_day > div.date > span").text
+                    eventMonth = int(importedDate.split('.')[0])
+                    adjustedYear = currentYear
+                    if currentMonth == 12 and eventMonth == 1:
+                        adjustedYear += 1
+                    date = f"{adjustedYear}.{importedDate}"
+
+                    gameTime = driver.find_element(By.CSS_SELECTOR, f"div.reserve_lst_bx > ul > li:nth-child({match}) > div.match_day > div.day_time > div.time > span").text
+                    mainTeamLogoImage = driver.find_element(By.CSS_SELECTOR, "div.team_logo > img").get_attribute("src")
+                    place = driver.find_element(By.CSS_SELECTOR, f"#reservation > div.reserve_lst_bx > ul > li:nth-child({match}) > div.match_team_info > div:nth-child(3) > div").text   
+                    link = url
+
+                    matchTourName = ""
+                    matchName = ""
+                    matchTotal = ""
+
+                    try:
+                        matchInfoFound = driver.find_element(By.CSS_SELECTOR, f"#reservation > div.reserve_lst_bx > ul > li:nth-child({match}) > div.match_team_info > div:nth-child(2)")
+                        childDivs = matchInfoFound.find_elements(By.TAG_NAME, "div")
+
+                        if len(childDivs) >= 2:
+                            matchTourName = childDivs[1].text
+                            if len(childDivs) >= 3:
+                                matchName = childDivs[2].text
+
+                        if matchTourName and matchName:
+                            matchTotal = matchTourName + " " + matchName
+                        elif matchTourName:
+                            matchTotal = matchTourName
+                        elif matchName:
+                            matchTotal = matchName
+
+                    except Exception as e:
+                        print(f"투어명/경기명 가져오기 실패: {e}")
+
+                    time.sleep(2)
+                    detailData = {
+                        'title': matchTotal,
+                        'company': "티켓링크",
+                        'link': link,
+                        'sdate': date,
+                        'edate': "",
+                        'place': place,
+                        'price': "",
+                        'grade': "전연령",
+                        'cast': "",
+                        'runningtime': "",
+                        'img': mainTeamLogoImage,
+                        'genre': genre,
+                        'etc': gameTime
+                    }
+                    time.sleep(2)
+                    print(detailData)
+                    detailDataList.append(detailData)
+
+                except Exception as e:
+                    print(f"{team_name} 경기정보 하나 가져오기 실패: {e}")
+
+        except Exception as e:
+            print(f"{team_name} 페이지 접속 또는 경기 리스트 가져오기 실패: {e}")
+
+    driver.quit()
+    return pd.DataFrame(detailDataList)
