dd543ee0ded92021b25084264b2da5bd6bae8db3
이가현 <gahyun970121@gmail.com>
2025-05-24 15:48:10 +0900

    update250524

--- crawling-master/interparkCrawler/crawler.py
+++ crawling-master/interparkCrawler/crawler.py
@@ -55,7 +55,7 @@ def ticket_info(driver, concert_ticket, visited_titles, start_idx, end_idx, curr
       sdate, edate = (period.split(" ~") if " ~" in period else (period, period))
       # edate가 '오픈런' 일때 값 변경하기
       if edate.strip() == '오픈런':
-        sdate = edate
+        edate = sdate
         
       place = driver.find_element(By.CSS_SELECTOR, ".infoBtn").text.replace("(자세히)", "")
       img = driver.find_element(By.CSS_SELECTOR, ".posterBoxImage").get_attribute("src")
